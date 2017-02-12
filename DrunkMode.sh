0af0b1ee2674bc4a64b1d1251d0544fa40eab515
Jakub Różycki <jakub.rozycki@outlook.com>
2017-02-12 22:14:43 +0100

    Added support for two input and error message

--- DrunkMode.sh
+++ DrunkMode.sh
@@ -4,9 +4,17 @@ trap '' 2
 
 while true
 do
-	echo -n $(whoami)@$(hostname):$(pwd)'# '
-	read -e command
-	$command
+        echo -n $(whoami)@$(hostname):$(pwd)'[0]# '
+        read -e command0
+        echo -n $(whoami)@$(hostname):$(pwd)'[1]# '
+        read -e command1
+
+        if [ "$command0" == "$command1" ] ; then
+                $command0
+        else
+                echo "Try again!"
+        fi
+
 done
 
 trap 2
